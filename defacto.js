c9a30e9ef1e36bfda53702cd943192e1d672ce0e
Mike Fuller <emailthemike@gmail.com>
2018-11-22 01:47:15 -0500

    added project files

---
+++ defacto.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+function Defacto(port=null,options={"staticPath":null,"jsonLimit":1024*1024*1,"maxAge":1000 * 10,"jsonBody":true,"cors":null}) {
+  if (!port || typeof port !== 'number') {
+    let err = new Error('Missing port!');
+    throw(err);
+    return err;
+  }
+
+  const app = express();
+
+  app.use(compression());
+  app.use(helmet());
+
+  if (options.cors && typeof options.cors === 'object') {
+    app.use(cors(options.cors));
+  }
+  if (options.staticPath && typeof options.staticPath === 'string') {
+    app.use(express.static(options.staticPath,{"maxAge":(options.maxAge||1000 * 10)}));
+  }
+
+  if (options.jsonBody) {
+    app.use(bodyParser.json({
+      "limit":(options.jsonLimit || 1024 * 1024 * 1)
+    }));
+    app.use((err,req,res,next)=>{
+      res.status(400).json({"code":400,"message":err.message||err.toString()});
+    });
+  }
+
+  const server = app.listen(port,(err)=>{
+    if (err) {
+      throw(err);
+    }
+  });
+  return {"app":app,"server":server};
+}
+
+module.exports = Defacto;
