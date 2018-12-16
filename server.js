1eea1e87252ac0d5931f15c60d21f2c141fd5d7e
Mike Fuller <emailthemike@gmail.com>
2018-12-15 21:23:45 -0500

    express now includes json body parser. removed bodyParser from server

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const compression = require('compression');
 const helmet = require('helmet');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 function Server(port=null,options={"staticPath":null,"jsonLimit":1024*1024*1,"maxAge":1000 * 10,"cors":null}) {
@@ -26,7 +25,7 @@ function Server(port=null,options={"staticPath":null,"jsonLimit":1024*1024*1,"ma
     app.use(express.static(options.staticPath,{"maxAge":(options.maxAge||1000 * 10)}));
   }
 
-  app.use(bodyParser.json({
+  app.use(express.json({
     "limit":(options.jsonLimit || 1024 * 1024 * 1)
   }));
 
